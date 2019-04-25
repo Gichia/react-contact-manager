2928880c7622121ab1d24e2b2645fb6cfa00d42f
Gichia <petergichia35@gmail.com>
2019-04-25 11:57:04 +0300

    Updates

---
+++ precache-manifest.4dc33dbd56294ad9a632f247dabce86f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e5ce45964cc5cda9993a",
+    "url": "/react-contact-manager/static/css/main.11d832ee.chunk.css"
+  },
+  {
+    "revision": "e5ce45964cc5cda9993a",
+    "url": "/react-contact-manager/static/js/main.e5754d97.chunk.js"
+  },
+  {
+    "revision": "817909a50868ee2241bd",
+    "url": "/react-contact-manager/static/js/runtime~main.f3ab48b1.js"
+  },
+  {
+    "revision": "bb0d4df24378deb74fa0",
+    "url": "/react-contact-manager/static/css/2.dc101e3d.chunk.css"
+  },
+  {
+    "revision": "bb0d4df24378deb74fa0",
+    "url": "/react-contact-manager/static/js/2.47db5fc6.chunk.js"
+  },
+  {
+    "revision": "c273d4fe8db93f7f971ba0319f3e366f",
+    "url": "/react-contact-manager/index.html"
+  }
+];
